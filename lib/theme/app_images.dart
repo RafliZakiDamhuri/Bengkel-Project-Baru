b7db8cf587b3fc561a7f44e839ce0525e025bfc2
BNI - rifqy adli damhuri <raflidamhuri977@gmail.com>
2025-12-12 20:13:43 +0700

    fixing bug 12122025

---
+++ lib/theme/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  AppImages();
+  String imageHomePage1 = 'assets/images/Screenshot 2025-12-11 181957.png';
+  String imageHomePage2 = 'assets/images/Screenshot 2025-12-11 151416.png';
+}
